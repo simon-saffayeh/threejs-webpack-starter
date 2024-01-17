9fb1dd3a56efc9d0f92222a00e2f8d45dd756905
simon-saffayeh <93934522+simon-saffayeh@users.noreply.github.com>
2024-01-17 10:15:14 -0500

    asdsa
    adsddas

--- src/script.js
+++ src/script.js
@@ -2,7 +2,9 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
-
+// Loading
+const textureLoader = new THREE.TextureLoader()
+const normalTexture = textureLoader.load('/textures/NormalMap.png')
 // Debug
 const gui = new dat.GUI()
 
@@ -18,10 +20,11 @@ const geometry = new THREE.SphereBufferGeometry( .5,64,64);
 // Materials
 
 const material = new THREE.MeshStandardMaterial()
-material.color = new THREE.Color(0xffff)
+material.color = new THREE.Color(0x292929)
 material.metalness = 0.7;
-material.roughness = 0.2
-material.wireframe = true
+material.roughness = 0.2;
+// material.wireframe = true
+material.normalMap = normalTexture
 
 // Mesh
 const sphere = new THREE.Mesh(geometry,material)
@@ -35,6 +38,33 @@ pointLight.position.y = 3
 pointLight.position.z = 4
 scene.add(pointLight)
 
+const pointLight2 = new THREE.PointLight(0xff0000, 3)
+pointLight2.position.set(4.7,-10.6,1)
+scene.add(pointLight2)
+
+
+
+const pointLight3 = new THREE.PointLight(0xff0000, 3)
+pointLight2.position.set(4.7,-10.6,1)
+scene.add(pointLight3)
+const light1_folder = gui.addFolder("Light 1")
+light1_folder.add(pointLight3.position, 'x').step(0.1).min(-10).max(10)
+light1_folder.add(pointLight3.position, 'y').step(0.1).min(-30).max(30)
+light1_folder.add(pointLight3.position, 'z').step(0.1).min(-10).max(10)
+light1_folder.add(pointLight3, 'intensity').step(0.1).min(0).max(10)
+
+
+const light_color = {
+    color: 0xff0
+}
+light1_folder.addColor(light_color, 'color')
+.onChange(() => {
+    pointLight3.color.set(light_color.color)
+})
+
+
+const pointLightHelper = new THREE.PointLightHelper(pointLight3,1)
+scene.add(pointLightHelper)
 /**
  * Sizes
  */
@@ -107,3 +137,5 @@ const tick = () =>
 }
 
 tick()
+
+//boxes choose a box
